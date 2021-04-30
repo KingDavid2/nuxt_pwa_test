5ea422f97d67182d54e1aec7a97c3f99597eebe4
David Nahum <nausito@gmail.com>
2021-04-30 12:24:14 -0600

    Left drawer

---
+++ components/LeftDrawer.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      v-click-outside="onClickOutside"
+      clipped
+      permanent
+      app
+    >
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            {{ item.icon }}
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+
+export default {
+
+  name: 'LeftDrawer',
+  data() {
+    return {
+      drawer: true,
+      mini: true,
+      items: [
+        { icon: 'I', title: 'Lorem ipsum' },
+        { icon: 'II', title: 'dolor sit amet' },
+        { icon: 'III', title: 'consectetur adipiscing elit' },
+      ],
+    }
+  },
+  created() {
+  },
+  methods: {
+    onClickOutside () {
+      this.mini = true
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,61 +1,19 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <LeftDrawer
+       />
     <v-app-bar
       :clipped-left="clipped"
+      dense
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+
+<v-spacer></v-spacer>
+
+      <v-toolbar-title center v-text="title" />
+<v-spacer></v-spacer>
+
     </v-app-bar>
     <v-main>
       <v-container>
@@ -89,10 +47,15 @@
 </template>
 
 <script>
+import LeftDrawer from '@/components/LeftDrawer'
+
 export default {
+  components: {
+    LeftDrawer
+  },
   data () {
     return {
-      clipped: false,
+      clipped: true,
       drawer: false,
       fixed: false,
       items: [
@@ -110,7 +73,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'INTERACTION OF COLOR'
     }
   }
 }
